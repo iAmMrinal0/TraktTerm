091d587d4f6c2701b50ed3d949eb27bf30ca57d6
Mrinal Purohit <mrinal1892005@gmail.com>
2016-02-19 22:04:01 +0530

    Code format

--- trakt.py
+++ trakt.py
@@ -18,60 +18,64 @@ def get_time():
     return final_date
 
 
-config = configparser.ConfigParser()
-config.read("config.ini")
-if not config["token"]["access_token"]:
-    print("Open the link in a browser and paste the pincode when prompted:")
-    print("https://trakt.tv/oauth/authorize?response_type=code&client_id={0}&"
-          "redirect_uri=urn:ietf:wg:oauth:2.0:oob".format(
-              config["token"]["client_id"]))
-    pincode = input("enter pincode:")
-    url = "https://api-v2launch.trakt.tv/oauth/token"
-    values = {
-        "code": pincode,
-        "client_id": config["token"]["client_id"],
-        "client_secret": config["token"]["client_secret"],
-        "redirect_uri": "urn:ietf:wg:oauth:2.0:oob",
-        "grant_type": "authorization_code"
+def main():
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    if not config["token"]["access_token"]:
+        print("Open the link in a browser and paste the pincode when prompted")
+        print("https://trakt.tv/oauth/authorize?response_type=code&"
+              "client_id={0}&redirect_uri=urn:ietf:wg:oauth:2.0:oob".format(
+                  config["token"]["client_id"]))
+        pincode = input("enter pincode:")
+        url = "https://api-v2launch.trakt.tv/oauth/token"
+        values = {
+            "code": pincode,
+            "client_id": config["token"]["client_id"],
+            "client_secret": config["token"]["client_secret"],
+            "redirect_uri": "urn:ietf:wg:oauth:2.0:oob",
+            "grant_type": "authorization_code"
+        }
+
+        req = requests.post(url, data=values)
+        resp = req.json()
+
+        config["token"]["access_token"] = resp["access_token"]
+        with open('config.ini', 'w') as configfile:
+            config.write(configfile)
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + config["token"]["access_token"],
+        'trakt-api-version': '2',
+        'trakt-api-key': config["token"]["client_id"]
     }
 
-    req = requests.post(url, data=values)
-    resp = req.json()
+    showname = input("enter show name:")
+    season_no = int(input("enter season number:"))
+    episode = int(input("enter episode number:"))
+    url = "https://api-v2launch.trakt.tv/calendars/my/shows/"
+    date_since = str(date.today() - timedelta(days=7))
+    days = "7"
+    resp = requests.get(url + date_since + "/" + days, headers=headers)
+    print(resp.status_code)
+    jso = resp.json()
+    vals = {}
+    for val in jso:
+        if showname.lower() in val["show"]["title"].lower():
+            vals["shows"] = [val["show"]]
+            vals["shows"][0]["seasons"] = [{}]
+            vals["shows"][0]["seasons"][0]["number"] = season_no
+            vals["shows"][0]["seasons"][0]["episodes"] = [{}]
+            vals["shows"][0]["seasons"][0]["episodes"][0][
+                "watched_at"] = get_time()
+            vals["shows"][0]["seasons"][0]["episodes"][0]["number"] = episode
+            break
 
-    config["token"]["access_token"] = resp["access_token"]
-    with open('config.ini', 'w') as configfile:
-        config.write(configfile)
+    url_sync = "https://api-v2launch.trakt.tv/sync/history"
 
-headers = {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer ' + config["token"]["access_token"],
-    'trakt-api-version': '2',
-    'trakt-api-key': config["token"]["client_id"]
-}
-
-showname = input("enter show name:")
-season_no = int(input("enter season number:"))
-episode = int(input("enter episode number:"))
-url = "https://api-v2launch.trakt.tv/calendars/my/shows/"
-date_since = str(date.today() - timedelta(days=7))
-days = "7"
-resp = requests.get(url + date_since + "/" + days, headers=headers)
-print(resp.status_code)
-jso = resp.json()
-vals = {}
-for val in jso:
-    if showname.lower() in val["show"]["title"].lower():
-        vals["shows"] = [val["show"]]
-        vals["shows"][0]["seasons"] = [{}]
-        vals["shows"][0]["seasons"][0]["number"] = season_no
-        vals["shows"][0]["seasons"][0]["episodes"] = [{}]
-        vals["shows"][0]["seasons"][0]["episodes"][0][
-            "watched_at"] = get_time()
-        vals["shows"][0]["seasons"][0]["episodes"][0]["number"] = episode
-        break
+    rep = requests.post(url_sync, data=json.dumps(vals), headers=headers)
+    print(rep.json(), rep.status_code)
 
 
-url_sync = "https://api-v2launch.trakt.tv/sync/history"
-
-rep = requests.post(url_sync, data=json.dumps(vals), headers=headers)
-print(rep.json(), rep.status_code)
+if __name__ == "__main__":
+    main()
